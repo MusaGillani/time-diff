634bcab63290c828e6c183ea8b552a6baecbc96e
musagillani <musagillaniwork@gmail.com>
2024-04-09 21:48:00 +0500

    feat(timeform): updated schema to render fields consecutive
    removed time input component

--- src/components/TimeForm.tsx
+++ src/components/TimeForm.tsx
@@ -6,133 +6,208 @@ import { z } from "zod";
 import { Form, FormControl, FormField, FormItem, FormMessage } from "./ui/form";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
-import { PlusIcon } from "lucide-react";
+import { MinusCircleIcon, PlusIcon } from "lucide-react";
 import { Label } from "./ui/label";
-import { format } from "@formkit/tempo";
+import { Separator } from "./ui/separator";
+
+const time = z.object({ hour: z.number().max(24), minute: z.number().max(59) });
+
+const afkBreak = z
+  .array(
+    z
+      .object({
+        away: time,
+        back: time,
+      })
+      .optional()
+  )
+  .min(1);
 
 const schema = z.object({
-  here: z.date(),
-  away: z.array(z.object({ time: z.date().optional() })).min(1),
-  back: z.array(z.object({ time: z.date().optional() })).min(1),
-  leaving: z.date(),
+  here: time,
+  afkBreak,
+  leaving: time,
 });
 
-type FormSchema = z.infer<typeof schema>;
+const defaultBreakObj = {
+  away: {
+    hour: 0,
+    minute: 0,
+  },
+  back: {
+    hour: 0,
+    minute: 0,
+  },
+};
+
+export type FormSchema = z.infer<typeof schema>;
 
 function TimeForm() {
   const form = useForm<FormSchema>({
     resolver: zodResolver(schema),
     defaultValues: {
-      away: [{}], // to show atleast one input field
-      back: [{}],
+      afkBreak: [defaultBreakObj],
     },
   });
 
-  const awayFields = useFieldArray({
+  const breakFields = useFieldArray({
     control: form.control,
-    name: "away",
-  });
-  const backFields = useFieldArray({
-    control: form.control,
-    name: "back",
+    name: "afkBreak",
   });
 
   function onSubmit(data: FormSchema) {}
 
-  function formatTime(value: string | undefined) {
-    if (!!value) return format(value, { time: "medium" });
-  }
   return (
     <div>
       <Form {...form}>
         <form
           onSubmit={form.handleSubmit(onSubmit)}
-          className="space-y-8 w-2/5 mx-auto gap-y-4 my-2"
+          className="space-y-8 w-2/5 mx-auto gap-y-4 my-5"
         >
           <Label className="text-2xl">Here</Label>
-          <FormField
-            control={form.control}
-            name="here"
-            render={({ field }) => (
-              <FormItem>
-                <FormControl>
-                  <Input
-                    {...field}
-                    placeholder="here"
-                    value={formatTime(field.value?.toISOString())}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <div className="flex items-center gap-x-10">
-            <Label className="text-2xl">Back</Label>
-            <Button type="button" onClick={() => backFields.append({})}>
-              <PlusIcon />
-            </Button>
-          </div>
-          {backFields.fields.map((field, index) => (
+          <div className="flex gap-x-5 ">
             <FormField
-              key={field.id}
               control={form.control}
-              name={`back.${index}.time`}
+              name="here.hour"
               render={({ field }) => (
                 <FormItem>
                   <FormControl>
-                    <Input
-                      {...field}
-                      placeholder="back"
-                      value={formatTime(field.value?.toISOString())}
-                    />
+                    <Input {...field} placeholder="here" />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
               )}
             />
-          ))}
-          <div className="flex items-center gap-x-10">
-            <Label className="text-2xl">Away</Label>
-            <Button type="button" onClick={() => awayFields.append({})}>
-              <PlusIcon />
-            </Button>
-          </div>
-          {awayFields.fields.map((field, index) => (
             <FormField
-              key={field.id}
               control={form.control}
-              name={`away.${index}.time`}
+              name="here.minute"
               render={({ field }) => (
                 <FormItem>
                   <FormControl>
-                    <Input
-                      {...field}
-                      placeholder="away"
-                      value={formatTime(field.value?.toISOString())}
-                    />
+                    <Input {...field} placeholder="here" />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
               )}
             />
+          </div>
+          <Separator />
+          <div className="flex items-center gap-x-10">
+            <Label className="text-2xl">Breaks</Label>
+            <Button
+              type="button"
+              onClick={() => breakFields.append(defaultBreakObj)}
+            >
+              <PlusIcon />
+            </Button>
+          </div>
+          {breakFields.fields.map((field, index) => (
+            <div
+              key={index}
+              className="border-2 border-slate-300 rounded-lg p-5 "
+            >
+              <Label className="text-xl">Away</Label>
+              <div className="flex gap-x-5 ">
+                <FormField
+                  control={form.control}
+                  name={`afkBreak.${index}.away.hour`}
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <Input {...field} placeholder="hour" />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name={`afkBreak.${index}.away.minute`}
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <Input
+                          {...field}
+                          placeholder="minutes"
+                          className="grow"
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <Separator />
+              <Label className="text-xl">Back</Label>
+              <div className="flex gap-x-5 ">
+                <FormField
+                  control={form.control}
+                  name={`afkBreak.${index}.back.hour`}
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <Input {...field} placeholder="hour" />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name={`afkBreak.${index}.back.minute`}
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <Input
+                          {...field}
+                          placeholder="minutes"
+                          className="grow"
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              {index > 0 && (
+                <Button
+                  className="my-5"
+                  onClick={() => breakFields.remove(index)}
+                  type="button"
+                >
+                  <MinusCircleIcon />
+                </Button>
+              )}
+            </div>
           ))}
-          <Label className="text-2xl">Leaving</Label>
-          <FormField
-            control={form.control}
-            name="leaving"
-            render={({ field }) => (
-              <FormItem>
-                <FormControl>
-                  <Input
-                    {...field}
-                    placeholder="leaving"
-                    value={formatTime(field.value?.toISOString())}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+          <Separator />
+          <Label className="text-2xl !mt-2">Leaving</Label>
+          <div className="flex gap-x-5 ">
+            <FormField
+              control={form.control}
+              name={`leaving.hour`}
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input {...field} placeholder="here" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name={`leaving.minute`}
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input {...field} placeholder="here" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
           <Button type="submit">Submit</Button>
         </form>
       </Form>
--- src/components/TimeInput.tsx
+++
@@ -1,5 +0,0 @@
-function TimeInput() {
-  return <div></div>;
-}
-
-export default TimeInput;
