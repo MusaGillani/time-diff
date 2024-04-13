14182683cfba5d50e1b16163ada1c3db88c437ec
musagillani <musagillaniwork@gmail.com>
2024-04-13 05:14:03 +0500

    feat: width in small screens to be ~ 90%
    fix width in small screens to be 90% instead of 100%

--- src/app/FormStack.tsx
+++ src/app/FormStack.tsx
@@ -9,7 +9,7 @@ import { Steps } from "@/state/store";
 function FormStack() {
   const step = useStateStore((store) => store.step);
   return (
-    <div className="sm:full relative m-auto my-5 h-fit max-h-114 md:w-3/5 lg:w-2/5">
+    <div className="relative m-auto my-5 h-fit max-h-114 sm:w-11/12 md:w-3/5 lg:w-2/5">
       <HereForm />
       {step !== Steps.HERE && <BreakForm />}
       {(step === Steps.LEAVING || step === Steps.RESULT) && <LeavingForm />}
