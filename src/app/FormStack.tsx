37d1405013cef3658fd30d800a6ab3d29b372f29
musagillani <musagillaniwork@gmail.com>
2024-04-13 05:12:35 +0500

    fix: className to w-3/5 instead of 3/5
    styles were not correct in md screens

--- src/app/FormStack.tsx
+++ src/app/FormStack.tsx
@@ -9,7 +9,7 @@ import { Steps } from "@/state/store";
 function FormStack() {
   const step = useStateStore((store) => store.step);
   return (
-    <div className="md:3/5 sm:full relative m-auto my-5 h-fit max-h-114 lg:w-2/5">
+    <div className="sm:full relative m-auto my-5 h-fit max-h-114 md:w-3/5 lg:w-2/5">
       <HereForm />
       {step !== Steps.HERE && <BreakForm />}
       {(step === Steps.LEAVING || step === Steps.RESULT) && <LeavingForm />}
